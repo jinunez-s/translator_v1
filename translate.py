e394b596dbeb108bf8929a0a4319dc342346d178
jinunez-s <julionr14@gmail.com>
2023-08-11 01:11:24 -0600

    First Commit

---
+++ translate.py
@@ -0,0 +1,30 @@
+#Python
+#pip3 install googletrans
+
+#importing libraries
+from googletrans import Translator, constants
+from pprint import pprint
+
+#interface from googletrans
+#init the Google API translator
+translator = Translator()
+
+#install newer version of googletrans
+# pip3 uninstall googletrans
+# pip3 install googletrans==3.1.0a0
+
+#translate a spanish text to arabic for instance
+translation = translator.translate("Hola mundo", dest="ar")
+print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+
+#translate more than phrase
+sentences = [
+    "Hello everyone",
+    "How are you ?",
+    "Do you speak english ?",
+    "Good bye!"
+]
+
+translations = translator.translate(sentences, dest="tr")
+for translation in translations:
+    print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
